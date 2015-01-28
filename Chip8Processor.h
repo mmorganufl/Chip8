242f720695125ebf1623192a0b96dee51f7a81a1
User <user@ubuntu.(none)>
2015-01-28 08:29:08 -0500

    added some documentation

--- Chip8Processor.h
+++ Chip8Processor.h
@@ -9,13 +9,54 @@
 class Chip8Processor
 {
     typedef uint16_t Chip8Register;
-    static const uint16_t RAM_SIZE = 4096;
+    static const uint16_t RAM_SIZE = 0x1000;  // 4k
+    static const uint16_t ROM_OFFSET = 0x200;
 
 public:
+    /**
+     * Constructor
+     */
     Chip8Processor();
+
+    /**
+     * Destructor
+     */
     virtual ~Chip8Processor();
 
+    /**
+     * Loads a ROM from a buffer and places it into RAM
+     * @param src The address of the ROM
+     * @length The length of the ROM in bytes
+     * @return Returns true if the ROM was successfully loaded into memory
+     */
     bool LoadRom(unsigned char* src, uint16_t length);
+
+    /**
+     * Begins execution at the current PC.  Reset should be called first
+     * to initialize all values.
+     * @return Returns true if the processor is started
+     */
+    bool Run();
+
+    /**
+     * Stops program execution
+     * @return Returns true if the processor is stopped
+     */
+    bool Stop();
+
+    /**
+     * Resets the internal state of the processor.  All registers are zeroed out,
+     * except the PC which is set to ROM_OFFSET.  All timers are reset and paused.
+     * @return True if the processor is reset
+     */
+    bool Reset();
+
+    /**
+     * Returns true if the processor is currently executing a program
+     * @return True if the processor is currently executing a program
+     */
+    bool IsRunning();
+
 protected:
     Chip8Register _v0;
     Chip8Register _v1;
@@ -35,6 +76,10 @@ protected:
     Chip8Register _vF;
     Chip8Register _pc;
     Chip8Register _I;
+
+    bool _isRunning;
+    bool Step();
+    bool HandleOpCode(uint16_t opcode);
 };
 
 #endif /* CHIP8PROCESSOR_H_ */
